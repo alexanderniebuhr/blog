dece090ae7badaaef0bd4bb89fc0953581a5b238
Alexander Niebuhr <alexander@nbhr.io>
2023-06-07 21:07:18 +0200

    wip

--- packages/db/src/schema/products.ts
+++ packages/db/src/schema/products.ts
@@ -1,6 +1,9 @@
 import type { InferModel } from 'drizzle-orm'
 import { doublePrecision, pgTable, text, uuid } from 'drizzle-orm/pg-core'
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import * as _drizzle from '../../node_modules/drizzle-orm/db.d-a6fe1b19.js'
+
 export const products = pgTable('products', {
 	id: uuid('id').primaryKey().defaultRandom(),
 	name: text('name'),
