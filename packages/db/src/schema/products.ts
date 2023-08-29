d6b045ec429abef516bf6ae2b3effac722056c86
Alexander Niebuhr <alexander@nbhr.io>
2023-08-29 21:01:08 +0200

    wip

--- packages/db/src/schema/products.ts
+++ packages/db/src/schema/products.ts
@@ -1,14 +1,13 @@
-import type { InferModel } from 'drizzle-orm'
-// import type { PgTableWithColumns, PgColumn } from "drizzle-orm/pg-core";
+import type { InferSelectModel, InferInsertModel } from "drizzle-orm";
 
-import { doublePrecision, pgTable, text, uuid } from 'drizzle-orm/pg-core'
+import { doublePrecision, pgTable, text, uuid } from "drizzle-orm/pg-core";
 
-export const products = pgTable('products', {
-	id: uuid('id').defaultRandom().primaryKey(),
-	name: text('name'),
-	description: text('description'),
-	price: doublePrecision('price'),
-})
+export const products = pgTable("products", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  name: text("name"),
+  description: text("description"),
+  price: doublePrecision("price"),
+});
 
-export type Product = InferModel<typeof products>
-export type NewProduct = InferModel<typeof products, 'insert'>
+export type Product = InferSelectModel<typeof products>;
+export type NewProduct = InferInsertModel<typeof products>;
